8c2ef17790e5643dd4df82b64dbc0d0280ae9abb
Divyansh Goel <divyanshgoel64@gmail.com>
2025-03-05 16:16:29 +0530

    hehe

--- quizAppProject/src/main/java/com/divyansh/quizAppProject/controller/questionController.java
+++ quizAppProject/src/main/java/com/divyansh/quizAppProject/controller/questionController.java
@@ -3,7 +3,6 @@ package com.divyansh.quizAppProject.controller;
 import com.divyansh.quizAppProject.Entity.QuizQuestions;
 import com.divyansh.quizAppProject.service.questionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
