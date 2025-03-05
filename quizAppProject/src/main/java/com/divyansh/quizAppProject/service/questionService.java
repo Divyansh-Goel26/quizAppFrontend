977049f4156bc59154feed1c295eec1b5d26b18c
Divyansh Goel <divyanshgoel64@gmail.com>
2025-03-05 16:21:20 +0530

    just

--- quizAppProject/src/main/java/com/divyansh/quizAppProject/service/questionService.java
+++ quizAppProject/src/main/java/com/divyansh/quizAppProject/service/questionService.java
@@ -4,7 +4,6 @@ import com.divyansh.quizAppProject.Entity.QuizQuestions;
 import com.divyansh.quizAppProject.repository.questionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
